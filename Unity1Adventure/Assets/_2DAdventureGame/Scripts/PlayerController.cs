ee31bbf408c91f191b6f6a91bea77e21f44b5783
Bohdan Volkov <bohdanvolkovv@gmail.com>
2026-06-03 09:22:01 +0200

    Lekce3-01

--- Unity1Adventure/Assets/_2DAdventureGame/Scripts/PlayerController.cs
+++ Unity1Adventure/Assets/_2DAdventureGame/Scripts/PlayerController.cs
@@ -4,19 +4,20 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     public InputAction action;
-    private float Speed = 0.01f;
+    private float Speed = 3f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        action.Enable();
     }
 
     // Update is called once per frame
     void Update()
     {
+        // Time.deltaTime je cas ktery uplyne mezi framy
         Vector2 move = action.ReadValue<Vector2>();
         Debug.Log(move);
-        Vector2 position = (Vector2)transform.position + move * Speed;
+        Vector2 position = (Vector2)transform.position + move * Speed * Time.deltaTime;
         transform.position = position;
     }
 }
