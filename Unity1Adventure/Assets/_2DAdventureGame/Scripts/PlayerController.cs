0d77fd64ec2e3a6258a07a72027f9b7de63acd4a
Bohdan Volkov <bohdan.volkov@giacom.com>
2026-06-15 20:29:34 +0200

    Lekce 5

--- Unity1Adventure/Assets/_2DAdventureGame/Scripts/PlayerController.cs
+++ Unity1Adventure/Assets/_2DAdventureGame/Scripts/PlayerController.cs
@@ -5,29 +5,35 @@ public class PlayerController : MonoBehaviour
 {
     public InputAction action;
     private float Speed = 3f;
+    public int maxHealth = 10;
+    private int currentHealth;
     Rigidbody2D rigidbody2D;
     Vector2 move;
-    void Awake()
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private void Start()
     {
         rigidbody2D = GetComponent<Rigidbody2D>();
-    }
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
         action.Enable();
+        currentHealth = 0;
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
         // Time.deltaTime je cas ktery uplyne mezi framy
         move = action.ReadValue<Vector2>();
         Debug.Log(move);
         
     }
-    void FixedUpdate()
+    private void FixedUpdate()
     {
         Vector2 position = (Vector2)transform.position + move * Speed * Time.deltaTime;
         rigidbody2D.MovePosition(position);
+    }
+    private void ChangeHealth(int amount)
+    {
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+        Debug.Log($"{currentHealth} / {maxHealth}");
     }
 }
