a1b62c260632aafdd6ab81d0ca2745b5c0260ab8
Bohdan Volkov <bohdan.volkov@giacom.com>
2026-06-22 17:40:36 +0200

    Lekce7-prep

--- Unity1Adventure/Assets/_2DAdventureGame/Scripts/HealthCollectible.cs
+++ Unity1Adventure/Assets/_2DAdventureGame/Scripts/HealthCollectible.cs
@@ -2,12 +2,14 @@ using UnityEngine;
 
 public class HealthCollectible : MonoBehaviour
 {
+    public int healAmount = 20;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         PlayerController controller = collision.GetComponent<PlayerController>();
         if(controller != null)
         {
-            controller.ChangeHealth(2);
+            controller.ChangeHealth(healAmount);
             Destroy(gameObject);
         }
     }
